1772f9f2b6686b9f93b669a1cd8f2fb8a3a16b94
MohamedAbdalhalem <mohamed3ab7alem@gmail.com>
2024-11-23 19:22:28 +0200

    completed

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var typed = new Typed('#text', {
+    
+    strings: [
+        'Designer',
+        'Develpor',
+        'Freelancer',
+        'Photographer',
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+    loopcount: Infinity,
+    showCursor: true,
+    cursorChar: '|',
+    autoInsertCss: true,
+
+});
